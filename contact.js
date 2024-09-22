499013d6fd99be66c43411899151ab12e7f77a82
Ankit Kumar <ankitkr1801@gmail.com>
2024-09-23 00:31:36 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,40 @@
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// Define a schema
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    phoneNo: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+// Create a model
+const Contact = mongoose.model('Contact', contactSchema);
+
+// Export the model
+module.exports = Contact;
